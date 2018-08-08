d5bf611b34ab5df1c7fdd5d6580fe3b5986e2716
Stephanie Lampotang <slampota@usc.edu>
2018-08-08 09:23:38 -0700

    xcode was messing up so ill just commit again now that its working

--- ItemShareApp/ItemShareApp/ViewControllers/Login/RegisterViewController.m
+++ ItemShareApp/ItemShareApp/ViewControllers/Login/RegisterViewController.m
@@ -5,6 +5,8 @@
 //  Created by Nicolas Machado on 7/26/18.
 //  Copyright © 2018 FBU-2018. All rights reserved.
 //
+// xcode was messing up so ill just commit again now that its working
+//
 
 #import "RegisterViewController.h"
 #import "User.h"
