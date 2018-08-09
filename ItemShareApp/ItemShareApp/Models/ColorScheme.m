0d844f556ea33515de40a620673860b9eb3fbfb1
Nicolas Machado <33378384+nmachado26@users.noreply.github.com>
2018-08-08 22:02:18 -0700

    finish color scheme update

--- ItemShareApp/ItemShareApp/Models/ColorScheme.m
+++ ItemShareApp/ItemShareApp/Models/ColorScheme.m
@@ -13,7 +13,8 @@
 //to implement: init the model property and call setColors
 - (void)setColors{
     //main COLOR
-    UIColor *mainColor = [UIColor colorWithRed:99.0f/255.0f green:99.0f/255.0f blue:255.0f/255.0f alpha:1.0f];
+    //
+    UIColor *mainColor = [UIColor colorWithRed:255.0f/255.0f green:98.0f/255.0f blue:68.0f/255.0f alpha:1.0f];
     //cyan color
     UIColor *secondColor = [UIColor colorWithRed:202.0f/255.0f green:255.0f/255.0f blue:244.0f/255.0f alpha:1.0f];
     //darkblue
--- ItemShareApp/ItemShareApp/ViewControllers/MainControllers/CatAndItemTableViewController.m
+++ ItemShareApp/ItemShareApp/ViewControllers/MainControllers/CatAndItemTableViewController.m
@@ -60,7 +60,7 @@
     return 180;
 }
 
-- (UIImage *)imageForEmptyDataSet:(UIScrollView *)scrollView{
+- (UIImage *)imageForEmptyDataSet:(UIScrollView *)scrollView {
     return [UIImage imageNamed:@"orange_f"];
 }
 
@@ -87,10 +87,10 @@
 }
 
 //either this or image for button
-//- (NSAttributedString *)buttonTitleForEmptyDataSet:(UIScrollView *)scrollView forState:(UIControlState)state{
-//    NSDictionary *attributes = @{NSFontAttributeName: [UIFont boldSystemFontOfSize:17.0f]};
-//    return [[NSAttributedString alloc] initWithString:@"Continue" attributes:attributes];
-//}
+- (NSAttributedString *)buttonTitleForEmptyDataSet:(UIScrollView *)scrollView forState:(UIControlState)state{
+    NSDictionary *attributes = @{NSFontAttributeName: [UIFont boldSystemFontOfSize:17.0f]};
+    return [[NSAttributedString alloc] initWithString:@"Continue" attributes:attributes];
+}
 
 //- (UIImage *)buttonImageForEmptyDataSet:(UIScrollView *)scrollView forState:(UIControlState)state{
 //    return [UIImage imageNamed:@"orange_f"];
