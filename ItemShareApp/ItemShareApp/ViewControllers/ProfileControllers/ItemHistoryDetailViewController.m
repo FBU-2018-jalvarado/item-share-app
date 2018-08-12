f547479908acc315dae7973bad047e847ca9587a
Tarini Singh <tarsingh@fb.com>
2018-08-12 13:12:55 -0700

    changed swipe table transition to border instead of 3d bc it looks cooler

--- ItemShareApp/ItemShareApp/ViewControllers/ProfileControllers/ItemHistoryDetailViewController.m
+++ ItemShareApp/ItemShareApp/ViewControllers/ProfileControllers/ItemHistoryDetailViewController.m
@@ -146,7 +146,7 @@
     // basic swipe config
     cell.rightButtons = @[[MGSwipeButton buttonWithTitle:@"" icon:[UIImage imageNamed:@"bin"] backgroundColor:[UIColor redColor]],
                           [MGSwipeButton buttonWithTitle:@"" icon:[UIImage imageNamed:@"qrcoderealrealrealreal"] backgroundColor:[UIColor colorWithRed:(211/255) green:(211/255) blue:(211/255) alpha:.2]]];
-    cell.rightSwipeSettings.transition = MGSwipeTransition3D;
+    cell.rightSwipeSettings.transition = MGSwipeTransitionBorder;
     
     // expansion config
     cell.rightExpansion.buttonIndex = 0;
